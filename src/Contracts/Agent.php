44c582af9aa4252b6d1db6fd7b93ed06c06bd17d
Hamzah <hamzahnafalah@gmail.com>
2025-05-06 14:25:53 +0700

    update modules

--- src/Contracts/Agent.php
+++ src/Contracts/Agent.php
@@ -5,7 +5,7 @@ namespace Hanafalah\ModuleAgent\Contracts;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
-use Hanafalah\LaravelSupport\Contracts\DataManagement;
+use Hanafalah\LaravelSupport\Contracts\Supports\DataManagement;
 
 interface Agent extends DataManagement
 {
